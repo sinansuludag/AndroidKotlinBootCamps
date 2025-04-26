216b2ae8f583345aff27da4201cb3cd63d4dd8b3
Sinan Suludağ <sinan.sldg23@gmail.com>
2025-04-26 10:01:28 +0300

    Odev1 yapildi

---
+++ app/src/main/java/com/example/androidbootcamps/AndroidBootCampsHomework/Degiskenler.kt
@@ -0,0 +1,82 @@
+﻿package com.example.androidbootcamps.AndroidBootCampsHomework
+
+fun main() {
+
+    val sehir ="Elazığ"
+    println("Şehir : $sehir")
+
+    val ulke="Türkiye"
+    println("Ülke : $ulke")
+
+    val telefon="05474587874"
+    println("Telefon : $telefon")
+
+    val postaKodu="23000"
+    println("Posta Kodu : $postaKodu")
+
+    val email="ahmet.1@gmail.com"
+    println("Email : $email")
+
+    val meslek="Bilgisayar Mühendisliği"
+    println("Meslek : $meslek")
+
+    val stokMiktari=20.0
+    println("Stok Miktarı : $stokMiktari")
+
+    val musteriAdi="Zeynep"
+    println("Müşteri Adı : $musteriAdi")
+
+    val bakiye=2000.5
+    println("Bakiye : $bakiye")
+
+    val dogumGunu="Pazartesi"
+    println("Doğum Günü : $dogumGunu")
+
+    val maas=20000.5
+    println("Maaş : $maas")
+
+    val medeniDurum="Bekar"
+    println("Medeni Durum : $medeniDurum")
+
+    val urunYorum="Çok iyi"
+    println("Ürün Yorumu : $urunYorum")
+
+    val odemeTarihi="01/12/2024"
+    println("Ödeme Tarihi : $odemeTarihi")
+
+    val siparisAdedi=25
+    println("Sipariş Adedi : $siparisAdedi")
+
+    val kitapAdi="Suç ve Ceza"
+    println("Kitap Adı : $kitapAdi")
+
+    val odaSayisi=5
+    println("Oda Sayısı : $odaSayisi")
+
+    val yemekFiyati=350.5f
+    println("Yemek Fiyatı : $yemekFiyati")
+
+    val videoSuresi=30
+    println("Video Süresi : $videoSuresi")
+
+    val urunPuani=78.5
+    println("Ürün Puanı : $urunPuani")
+
+    val dosyaFormati="png"
+    println("Dosya Formatı : $dosyaFormati")
+
+    val telefonModeli="IPhone 15"
+    println("Telefon Modeli : $telefonModeli")
+
+    val tatilGunu="Pazar"
+    println("Tatil Günü : $tatilGunu")
+
+    val sokakAdi="Kelebek Sokak"
+    println("Sokak Adı : $sokakAdi")
+
+    val kalanDakika=22
+    println("Kalan Dakika : $kalanDakika")
+
+    val kuponKodu="bhbdbsncıjıd555"
+    println("Kupon Kodu : $kuponKodu")
+}
---
+++ app/src/main/java/com/example/androidbootcamps/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.androidbootcamps
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var sayi1:Int=0
+        var sayi2:Int=5
+
+        var sonuc:Int=sayi1+sayi2
+
+        println("Sonuc : $sonuc")
+
+    }
+}
